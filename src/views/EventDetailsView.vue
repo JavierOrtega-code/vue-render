3174bf3d93544f5915b5d858302258866ac5fcad
Javier Ortega Fernandez <javierortegafernandez0@gmail.com>
2026-03-19 08:40:05 +0100

    Vue 3 Reder with GitHub

--- src/views/EventDetailsView.vue
+++ src/views/EventDetailsView.vue
@@ -1,5 +1,33 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted} from 'vue'
+import EventService from '@/services/EventService'
 
-<template></template>
+const props = defineProps({
+  id: {
+    required: true,
+  }
+})
 
-<style scoped></style>
+const event = ref(null)
+
+
+onMounted(() => {
+  // fetch even by id and set local data
+  EventService.getEvent(props.id)
+    .then((response) => {
+      event.value = response.data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
+
+</script>
+
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
